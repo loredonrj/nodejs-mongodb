bfb9fd8ca801c7c213fd37bb7b6802199a22cbba
loredonrj <loredonrj@users.noreply.github.com>
2023-06-08 12:47:48 -0400

    Add files via upload

---
+++ controllers/Blog/blogCategoryController.js
@@ -0,0 +1,29 @@
+import { blogCategoryModel } from "../models/Blog/blogCategoryModel.js";
+import slugify from "slugify";
+
+//createBlogPostCategory --------------------------------
+const postCategory = async (req, res) => {
+    const { title } = req.body;
+    try {
+        if (title) {
+            req.body.slug = slugify(title.toLowerCase());
+            const category = await blogCategoryModel.create(req.body);
+            res.status(200).json({ status: true, message: 'category created susscessfully', category });
+        }
+    } catch (error) {
+        res.status(400).json({ status: false, message: 'something went wrong' });
+
+        
+    }
+}
+
+//getBlogPostsCategory (all posts) --------------------------------
+
+//getBlogPostCategory (single category) --we can find him by any of the fields of the Model --
+
+//deleteBlogPostCategory --------------------------------
+
+//updateBlogPostCategory --------------------------------
+
+
+export { postCategory };
---
+++ controllers/Blog/blogController.js
@@ -0,0 +1,42 @@
+import { Blog } from "../../models/Blog/blogModel.js";
+
+//createBlogPost --------------------------------
+const postBlog = async (req, res) => {
+  try {
+    const post = await Blog.create(req.body);
+    res.json(post);
+  } catch (error) {
+    res.json({ error: error });
+  }
+};
+
+//get Blog creator  --------------------------------
+
+const getBlog = async (req, res) => {
+  try {
+    const post = await Blog.find().populate("postedBy"); //we populate the post variable with the user data in the postedBy field (which is is an object)
+    res.json(post);
+  } catch (error) {
+    res.json({ error: error });
+  }
+};
+
+//TODO :
+//deleteBlogPost --------------------------------
+
+//updateBlogPost --------------------------------
+
+//getBlogPosts (all posts) --------------------------------
+
+//getBlogPost (single use) --we can find him by any of the fields of the Model -----
+
+
+export { postBlog, getBlog };
+
+/* es6 style: 
+module.exports = {
+  postBlog,
+  getBlog,
+};
+*/
+
