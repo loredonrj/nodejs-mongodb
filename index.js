cbe14d305f21091d50f4651ced6cb2ba97bc6644
loredonrj <loredonrj@users.noreply.github.com>
2023-06-08 12:15:32 -0400

    added files by upload from phone

--- index.js
+++ index.js
@@ -1,18 +1,32 @@
-import mongoose from "mongoose";
-import app from "./app.js";
+//step 1 : Create Server --------------------------------
+const express = require("express");
+const app = express();
+const PORT = process.env.PORT || 5000;
 
-( async () => {
-    try {
-        await mongoose.connect("mongodb://localhost:27017/gitsetup")
-        console.log("DB CONNECTED");
+app.use(bodyParser.json());
 
-        const onListening = () => {
-            console.log("Server is listening on port 5000 maybe");
-        }
+import BlogCategoryRouter from "./routes/Blog/blogCategoryRoute";
+//step 17 : Import userRouter
+import userRouter from "./routes/Users/userRoute";
 
-        app.listen(5000, onListening)
-    } catch (error) {
-        console.error("error: ", error);
-        throw error
-    }
-} ) ()
+app.use("/api/user", userRouter);
+app.use("/api/blog", blogRouter);
+app.use("/api/blog-category", BlogCategoryRouter);
+
+//step 2 and 3 : Connect database and start server --------------------------------
+
+(async () => {
+  try {
+    await mongoose.connect("mongodb://localhost:27017/gitsetup");
+    console.log("DB CONNECTED");
+
+    const onListening = () => {
+      console.log(`Server is listening at //localhost:${PORT}`);
+    };
+
+    app.listen(5000, onListening);
+  } catch (error) {
+    console.error("error: ", error);
+    throw error;
+  }
+})();
